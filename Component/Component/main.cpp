2e5a19b066c13604c7ddaa9b46f98c7d7bf0f841
jiangyongxu <jiangyongxu@zqykj.com>
2022-06-27 19:45:50 +0800

    chore(Component):temp

--- Component/Component/main.cpp
+++ Component/Component/main.cpp
@@ -1,20 +1,16 @@
-#include "combobox.h"
-#include <QtWidgets/QApplication>
+#include "mainwindow.h"
+
+#include <QApplication>
 #include <QFile>
-#include <QDebug>
+
 int main(int argc, char *argv[])
 {
-	QApplication a(argc, argv);
-	ComboBox w;
-	QFile qss(":/stylesheet.qss");
-	if (qss.open(QFile::ReadOnly)) {
-		QString style = QLatin1String(qss.readAll());
-		qDebug() << "Successed to read stylesheet qss";
-		qDebug() << style;
-		a.setStyleSheet(style);
-		qss.close();
-	}
-	else qDebug() << "Failed to read stylesheet.qss";
-	w.show();
-	return a.exec();
+    QApplication a(argc, argv);
+    QFile file(QString(":/qss/mainwindow.qss"));
+    file.open(QFile::ReadOnly);
+    MainWindow w;
+    w.setStyleSheet(file.readAll());
+    file.close();
+    w.show();
+    return a.exec();
 }
